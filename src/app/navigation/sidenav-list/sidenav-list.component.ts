07c2a9cd858967046500a3159df3ffb80ebb4806
adjaythakoerdien <a.thakoerdien@gmail.com>
2022-03-18 07:03:49 +0100

    Logout and redirection added

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,19 +1,43 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  EventEmitter,
+  Output,
+  OnDestroy,
+} from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
-  styleUrls: ['./sidenav-list.component.scss']
+  styleUrls: ['./sidenav-list.component.scss'],
 })
-export class SidenavListComponent implements OnInit {
+export class SidenavListComponent implements OnInit, OnDestroy {
   @Output() closeSidenav = new EventEmitter<void>();
+  isAuth = false;
+  authSubscription: Subscription;
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.authSubscription = this.authService.authChange.subscribe(
+      (authStatus) => {
+        this.isAuth = authStatus;
+      }
+    );
+  }
+
+  onLogout() {
+    this.authService.logout();
+    this.onClose();
+  }
+
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe();
   }
 
   onClose() {
-  this.closeSidenav.emit();
+    this.closeSidenav.emit();
   }
 }
