1b4b75ffe5694de9a27107cbd874cdeb3d2e2f77
adjaythakoerdien <a.thakoerdien@gmail.com>
2022-03-19 11:26:38 +0100

    Active exercise controls added

--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject, Subscription } from 'rxjs';
+import { TrainingService } from './training.service';
 
 @Component({
   selector: 'app-training',
   templateUrl: './training.component.html',
-  styleUrls: ['./training.component.scss']
+  styleUrls: ['./training.component.scss'],
 })
 export class TrainingComponent implements OnInit {
   ongoingTraining = false;
+  exerciseSubscription: Subscription;
 
-  constructor() { }
+  constructor(private trainingService: TrainingService) {}
 
   ngOnInit(): void {
+    this.exerciseSubscription = this.trainingService.exerciseChanged.subscribe(
+      (exercise) => {
+        exercise
+          ? (this.ongoingTraining = true)
+          : (this.ongoingTraining = false);
+      }
+    );
   }
-
 }
--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -1,6 +1,9 @@
 import { Exercise } from './exercise.model';
+import { Subject } from 'rxjs';
 
 export class TrainingService {
+  exerciseChanged = new Subject<Exercise>();
+
   private availableExercises: Exercise[] = [
     { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
     { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 10 },
@@ -8,8 +11,21 @@ export class TrainingService {
     { id: 'burpees', name: 'Burpees', duration: 60, calories: 10 },
     { id: 'jumping-jacks', name: 'Jumping Jacks', duration: 60, calories: 10 },
   ];
+  private runningExercise: Exercise | undefined;
+  private exercises: Exercise[] = [];
 
   getAvailableExercises(): Exercise[] {
     return this.availableExercises.slice();
+  }
+
+  startExercise(selectedId: string) {
+    this.runningExercise = this.availableExercises.find(
+      (ex) => ex.id === selectedId
+    );
+    this.exerciseChanged.next({ ...this.runningExercise });
+  }
+
+  getRunningExercise() {
+    return { ...this.runningExercise };
   }
 }
